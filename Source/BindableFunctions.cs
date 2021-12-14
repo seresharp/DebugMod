d89302e9dda398e88858b88b2cf66dfd1d0b42d2
cerpintext <cerpintext@protonmail.com>
2021-12-14 22:58:36 +0100

    yoinked flib's demasker func

--- Source/BindableFunctions.cs
+++ Source/BindableFunctions.cs
@@ -335,6 +335,43 @@ namespace DebugMod
             HeroController.instance.vignette.enabled = !HeroController.instance.vignette.enabled;
         }
 
+        [BindableMethod(name = "Deactivate Visual Masks", category = "Visual")]
+        public static void DeactivateVisualMasks() {
+            int ctr = 0;
+
+            void disableMask(GameObject go) {
+                foreach (Renderer r in go.GetComponentsInChildren<Renderer>()) {
+                    if (r.enabled) {
+                        ctr++;
+                        r.enabled = false;
+                    }
+                }
+            }
+
+            float knightZ = HeroController.instance.transform.position.z;
+            foreach (GameObject go in GameObject.FindObjectsOfType<GameObject>()) {
+                if (go.transform.position.z > knightZ) continue;
+
+                // A collection of ways to identify masks. It's possible some slip through the cracks I guess
+                if (go.name.StartsWith("msk_"))
+                    disableMask(go);
+                else if (go.name.StartsWith("Tut_msk"))
+                    disableMask(go);
+                else if (go.name.StartsWith("black_solid"))
+                    disableMask(go);
+                else if (go.name.ToLower().Contains("vignette"))
+                    disableMask(go);
+                else if (go.LocateMyFSM("unmasker") is PlayMakerFSM)
+                    disableMask(go);
+                else if (go.LocateMyFSM("remasker_inverse") is PlayMakerFSM)
+                    disableMask(go);
+                else if (go.LocateMyFSM("remasker") is PlayMakerFSM)
+                    disableMask(go);
+            }
+
+            Console.AddLine($"Deactivated {ctr} masks");
+        }
+
         [BindableMethod(name = "Toggle Hero Light", category = "Visual")]
         public static void ToggleHeroLight()
         {
